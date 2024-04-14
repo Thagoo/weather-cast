0502a7fa888ebfbac49b0891af22652d180029dc
thagoo <lohitgowda56@gmail.com>
2024-04-14 18:41:59 +0530

    Add getForecast functiont to fetch 5 days forecast and add ravalidation.
    - Server side

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,4 +1,7 @@
 "use server";
+
+import { groupForecastByDate } from "./utils";
+
 const OPENDATASOFT_API = process.env.OPENDATASOFT_API;
 const OPENWEATHER_API = process.env.OPENWEATHER_API;
 
@@ -8,9 +11,9 @@ export const getWeather = async (
   units: string = "metric"
 ) => {
   try {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API}&units=${units}`;
+    let urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API}&units=${units}`;
 
-    const response = await fetch(url);
+    const response = await fetch(urlWeather, { next: { revalidate: 600 } });
 
     const data = await response.json();
     return data;
@@ -20,6 +23,29 @@ export const getWeather = async (
   }
 };
 
+export const getforecast = async (
+  lat: number,
+  lon: number,
+  units: string = "metric"
+) => {
+  try {
+    let urlForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API}&units=${units}`;
+
+    const response = await fetch(urlForecast, { next: { revalidate: 600 } });
+
+    const data = await response.json();
+
+    const filteredData = {
+      ...data,
+      list: groupForecastByDate(data),
+    };
+
+    return filteredData;
+  } catch (error: any) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
 export const getCityList = async () => {
   try {
     const response = await fetch(`${OPENDATASOFT_API}?limit=100`);
