5f6b3e55ff254c2a54a93f4f6ec13b4cb0245214
thagoo <lohitgowda56@gmail.com>
2024-04-15 11:22:12 +0530

    Fix Padding in small devices

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="px-2 md:px-48 gap-4 flex flex-row  p-1">
+    <div className="px-2 md:px-48 md:gap-4 gap-1 flex flex-row  p-1">
       <Link href={"/"} className="flex-grow">
         <p className="text-2xl text-slate-900 dark:text-white font-semibold ">
           {" "}
